e4b03ff4047148bc2196e54b4d5dbaef3da9e7fe
smruti294 <163741887+smruti294@users.noreply.github.com>
2024-06-04 10:12:08 +0530

    Add files via upload

---
+++ Rating.java
@@ -0,0 +1,23 @@
+// Rating.java
+package com.example.outfitrating.model;
+
+public class Rating {
+    private String outfitName;
+    private int rating;
+
+    public String getOutfitName() {
+        return outfitName;
+    }
+
+    public void setOutfitName(String outfitName) {
+        this.outfitName = outfitName;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+}
---
+++ outfitratingapplication.java
@@ -0,0 +1,12 @@
+// OutfitRatingApplication.java
+package com.example.outfitrating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutfitRatingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OutfitRatingApplication.class, args);
+    }
+}
